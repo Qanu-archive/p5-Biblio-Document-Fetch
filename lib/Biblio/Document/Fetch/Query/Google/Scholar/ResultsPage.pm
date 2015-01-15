f4f4479100abd3a583d9841a04d2f8aeba454ba6
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-01-14 22:39:40 -0600

    add semi-colon for try-catch

--- lib/Biblio/Document/Fetch/Query/Google/Scholar/ResultsPage.pm
+++ lib/Biblio/Document/Fetch/Query/Google/Scholar/ResultsPage.pm
@@ -34,7 +34,7 @@ sub _build_next_page {
 	my @next_link = $tree->findnodes('//span[contains(@class,"gs_ico_nav_next")]/parent::*');
 	return undef unless @next_link;
 	my $next_uri = URI->new_abs( $next_link[0]->attr('href'), $self->response->base );
-	my $res = try { $self->query->get($next_uri); } catch { carp "Not able to retrieve next page: $_"; }
+	my $res = try { $self->query->get($next_uri); } catch { carp "Not able to retrieve next page: $_"; };
 	my $results;
 	$results = Biblio::Document::Fetch::Query::Google::Scholar::ResultsPage->new( query => $self->query,
 		response => $res ) if defined $res;
@@ -49,7 +49,7 @@ sub _build_previous_page {
 	my @prev_link = $tree->findnodes('//a/span[contains(@class,"gs_ico_nav_previous")]/parent::*');
 	return undef unless @prev_link;
 	my $prev_uri = URI->new_abs( $prev_link[0]->attr('href'), $self->response->base );
-	my $res = try { $self->query->get($prev_uri); } catch { carp "Not able to retrieve previous page: $_"; }
+	my $res = try { $self->query->get($prev_uri); } catch { carp "Not able to retrieve previous page: $_"; };
 	my $results;
 	$results = Biblio::Document::Fetch::Query::Google::Scholar::ResultsPage->new( query => $self->query,
 		response => $res) if defined $res;
